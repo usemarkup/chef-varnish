fd8dca596c9b995d0817b72b42e79a7ff28dce06
Gavin Staniforth <gavin@usemarkup.com>
2020-05-19 17:16:03 +0100

    feat: correct lock

--- recipes/install.rb
+++ recipes/install.rb
@@ -17,7 +17,7 @@ unless node['varnish']['package_version'].nil?
 
   yum_version_lock 'varnish' do
     version node['varnish']['package_version']
-    release '*'
+    release '1'
     action :add
   end
 end
